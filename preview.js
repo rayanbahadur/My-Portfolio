e21895c7176900bb1a771873ff4c50eff251c2ac
Rayan Bahadur <r.bahadur@se22.qmul.ac.uk>
2023-04-26 17:25:09 +0100

    Pretty much done I think

--- preview.js
+++ preview.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const previewBtn = document.getElementById("previewBtn");
     const previewDiv = document.getElementById("preview");
-    const editBtn = document.getElementById("editBtn");
 
     previewDiv.style.display = 'none';
+
     previewBtn.addEventListener("click", function (event) {
         const title = document.getElementById("title").value;
         const description = document.getElementById("description").value;
         const date = new Date().toLocaleString();
-        event.preventDefault()// prevent the form from submitting
+        event.preventDefault(); // prevent the form from submitting
         previewDiv.innerHTML = `
         <h3>${title}</h3>
         <h5>${date}</h5>
@@ -22,7 +22,13 @@ document.addEventListener('DOMContentLoaded', function() {
             </div>
         </form>
         `;
+        const editBtn = document.getElementById("editBtn");
+        editBtn.addEventListener("click", function (event) {
+            event.preventDefault();
+            previewDiv.innerHTML = `
+                <p id="editAlert">Please make changes in the "Add Post" section on the right and press preview to see the changes or post to add your entry <ion-icon id="iconP" name="arrow-forward-circle"></ion-icon></p>
+            `;
+        });
         previewDiv.style.display = 'block';
     });
-
 });
